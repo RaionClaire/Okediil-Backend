88bf10013712adcd2e166244c5df66740440da5a
Rio Ananda Putra <112097226+rioanandaputra-bot@users.noreply.github.com>
2025-10-20 00:32:59 +0700

    Fix casing of model reference and update source chat

--- database/seeders/SeederCRM.php
+++ database/seeders/SeederCRM.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
-use App\Models\CRM;
+use App\Models\Crm;
 
 class SeederCRM extends Seeder
 {
@@ -14,14 +14,14 @@ class SeederCRM extends Seeder
         $faker = Faker::create('id_ID');
 
         collect(range(1, 20))->each(function ($i) use ($faker) {
-            CRM::create([
+            Crm::create([
                 'tanggal' => $faker->date(),
                 'nama' => $faker->name,
                 'no_hp' => preg_replace('/[^\d]/', '', $faker->phoneNumber),
                 'alamat' => $faker->randomElement(['Kedamaian', 'Rajabasa', 'Tanjung Karang', 'Teluk Betung', 'Way Halim', 'Sukarame', 'Kemiling', 'Enggal', 'Gedong Air', 'Lainnya']),
                 'jenis_kelamin' => $faker->randomElement(['Laki-Laki', 'Perempuan']),
                 'pekerjaan' => $faker->randomElement(['Pelajar', 'Mahasiswa', 'Umum', 'Pekerja']),
-                'sumber_chat' => $faker->randomElement(['Instagram', 'WhatsApp']),
+                'sumber_chat' => $faker->randomElement(['Instagram', 'Whatsapp']),
                 'jenis_produk' => $faker->randomElement(['HP', 'Laptop']),
                 'kondisi' => $faker->randomElement(['Mati total', 'Nyala tapi tidak berfungsi', 'Layar pecah', 'Baterai cepat habis', 'Masalah pada aplikasi', 'Masalah pada jaringan']),
                 'merk' => $faker->randomElement(['Apple', 'Samsung', 'Xiaomi', 'Oppo', 'Vivo', 'Realme', 'Asus', 'Lenovo', 'Huawei', 'Lainnya']),
